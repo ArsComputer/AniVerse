1e3f526c50a5da2e1743dad8f59f7c6909ba46c3
okComputer <mohrasyaar2008@gmail.com>
2026-08-09 23:23:04 +0700

    Improve more styles: css + SVG

--- src/main.js
+++ src/main.js
@@ -10,19 +10,24 @@ document.querySelector("#app").innerHTML = `
 <header>
   <nav class="navbar">
     <div class="logo">AniVerse</div>
-    
-    .
 
-    <div class="cari-container">
-    <form class="form-cari" role="search">
-      <input
-        type="text"
-        placeholder="Cari Anime..."
-        class="input-cari" id="input-cari"
-        aria-label="Cari Anime"
-      />
-      <button type="submit" class="btn-cari" id="btn-cari">Cari</button>
-    </form>
+    <div className="cari-container">
+      <label for="input-cari" class="icon">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
+      </label>
+
+      <div class="cari-wrapper">
+        <form class="form-cari" role="search">
+          <input
+            type="text"
+            placeholder="Cari Anime..."
+            class="input-cari" id="input-cari"
+            aria-label="Cari Anime"
+          />
+
+          <button type="submit" class="btn-cari" id="btn-cari">Cari</button>
+        </form>
+      </div>
     </div>
   </nav>
 </header>
@@ -79,10 +84,10 @@ async function searchAnime(keyword) {
     });
 
   } catch (error) {
-    if (error.name !== "AbortError") return;
+    if (error.name === "AbortError") return;
 
     console.error("Gagal:", error);
-    createErrorState(error.message, daftarAnimeContainer, 'muted', 'state');
+    createErrorState(error.message, daftarAnimeContainer, "muted", "state");
   }
 }
 
