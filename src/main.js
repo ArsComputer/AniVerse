89be4a1debe04a7ca764ebfe5f2e435995ff28c7
okComputer <mohrasyaar2008@gmail.com>
2026-08-10 18:44:00 +0700

    Improving responsive style and animation for search form

--- src/main.js
+++ src/main.js
@@ -11,8 +11,8 @@ document.querySelector("#app").innerHTML = `
   <nav class="navbar">
     <div class="logo">AniVerse</div>
 
-    <div className="cari-container">
-      <label for="input-cari" class="icon">
+    <div class="cari-container">
+      <label for="input-cari" class="icon" id="menu-cari">
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
       </label>
 
@@ -100,3 +100,11 @@ btnCari.addEventListener('click', (e) => {
   e.preventDefault();
   searchAnime(inputCari.value.trim());
 });
+
+const menuCari = document.getElementById('menu-cari');
+const cariWrapper = document.querySelector('.cari-wrapper');
+
+menuCari.addEventListener('click', () => {
+  // cariWrapper.style.display = 'flex';
+  cariWrapper.classList.toggle('cari-open');
+})
