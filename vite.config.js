f1023f6fe3d0951f218f23a74fe872ad726beeba
okComputer <mohrasyaar2008@gmail.com>
2026-08-30 21:07:28 +0700

    Adding Vercel default path in vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import process from 'node:process';
 
-export default defineConfig({
-  base: "/AniVerse/",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: env.VERCEL ? "/" : "/AniVerse/",
+  }
 });
